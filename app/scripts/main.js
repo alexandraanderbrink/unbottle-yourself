25b6912bdaacc65fe7d152a99a140df0ddc9dae8
Alexandra Anderbrink <alexandra.anderbrink@monterosa.se>
2015-12-07 15:25:18 +0100

    test new repo

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,3 +7,20 @@ utils.addHelperClasses();
 
 utils.log('is mobile',utils.isMobile);
 utils.log('is smartphone',utils.isSmartphone);
+
+
+
+
+function timeConverter(UNIX_timestamp){
+  var a = new Date(UNIX_timestamp * 1000);
+  var date = a.getDate();
+  var hour = a.getHours();
+  var min = a.getMinutes();
+  var sec = a.getSeconds();
+  var time = date + ' ' + hour + ':' + min + ':' + sec ;
+  return time;
+  console.log(time);
+}
+
+
+
