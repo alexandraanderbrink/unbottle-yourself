6cfda0f70031cff937f5a5711c3156b5b06a8690
Alexandra Anderbrink <alexandra.anderbrink@monterosa.se>
2016-01-08 09:33:53 +0100

    adding files

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,66 +1,45 @@
+(function() {
 
-//$.ajax({
-//	url: "http://unbottle.getsandbox.com/campaign",
-//	contentType: "application/json",
-//}).done(function(data) {
-//	console.log(data);
-//	//console.log(data.start_date);
-//	var start_date = data.start_date;
-//});
-
-
-//Get the data from REST API
-jQuery.extend({
-	getValues: function(url) {
-		var startDate = null;
-		$.ajax({
-			type: 'get',
-			url: "http://unbottle.getsandbox.com/campaign",
-			dataType: 'json',
-			contentType: "application/json",
-			async: false,
-			success: function(data) {
-				startDate = data.start_date;
-			}
-		});
-		return startDate;
-	}
-});
-var unixTime = $.getValues();
-//Print the data from the Endpoint - in UNIX
-console.log("Unix: " + unixTime);
-
-//Converting into ISO
-var dateISO = new Date(unixTime).toISOString();
-dateISO; // => "1970-01-17T12:17:24.000Z"
-console.log("dateISO: " + dateISO);
-
-//Converting into YYYY-MM-DD
-function formatDate(date) {
-    var d = new Date(dateISO),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
-
-    if (month.length < 2) month = '0' + month;
-    if (day.length < 2) day = '0' + day;
-
-    return [year, month, day].join('-');
+//Get the start_date data from the endpoint url
+//with a function that returns the start_date value
+//from the ajax request
+function getValues(url) {
+	var startDate = null;
+	$.ajax({
+		type: 'get',
+		url: "http://unbottle.getsandbox.com/campaign",
+		dataType: 'json',
+		contentType: "application/json",
+		async: false,
+		success: function(data) {
+			//picking the start_date parameter
+			startDate = data.start_date;
+		}
+	});
+	return startDate;
 }
-console.log(formatDate(dateISO));
-
-//
-var deadline = '2016-01-01';
-var deadline = formatDate(dateISO);
-
+//Saving the return value from the getValues()
+//in a variable unixDate
+var unixDate = getValues();
+console.log("Unix date: " + unixDate);
 
 
+//Converting the unix date into Javascript Date object
+var unixToDate = new Date(unixDate * 1000);
+var endtime = unixToDate.toGMTString();
+console.log("Endtime: " + endtime);
+
+//Calculation of the time remaining in days,
+//hours, minutes and seconds
 function getTimeRemaining(endtime){
+	//Date.parse function is native JavaScript that
+	//converts a time string into a value in milliseconds
 	var t = Date.parse(endtime) - Date.parse(new Date());
 	var seconds = Math.floor( (t/1000) % 60 );
 	var minutes = Math.floor( (t/1000/60) % 60 );
 	var hours = Math.floor( (t/(1000*60*60)) % 24 );
 	var days = Math.floor( t/(1000*60*60*24) );
+	//Output the data as a reusable object
 	return {
 		'total': t,
 		'days': days,
@@ -70,54 +49,43 @@ function getTimeRemaining(endtime){
 	};
 }
 
-function initializeSec(id, endtime){
-	var sec = document.getElementById(id);
-	var timeinterval = setInterval(function(){
-	    var t = getTimeRemaining(endtime);
-		if(t.seconds<10){
-			sec.innerHTML = '0' + t.seconds;
-		}else {
-			sec.innerHTML = t.seconds;
-		}
-		if(t.total<=0){
-			clearInterval(timeinterval);
-		}
-		if(!t.seconds) {
-			sec.innerHTML = "--";
-		}
-		if(t.seconds<=0) {
-			sec.innerHTML = "00";
-		}
-	},1000);
-}
-initializeSec('sec', deadline);
 
-
-
-function initializeMin(id, endtime){
-	var min = document.getElementById(id);
+//Calculate the days remaining, printing it
+//in the days div
+function initializeDays(id, endtime){
+	var days = document.getElementById(id);
+	//The timeinterval is set to
+	//calculate the remaining time in days.
+	//and run it once every second
+	//Output the remaining time in days to our div.
 	var timeinterval = setInterval(function(){
 		var t = getTimeRemaining(endtime);
-		if(t.minutes<10){
-			min.innerHTML = '0' + t.minutes;
+		//If the days are less than 10 add a 0
+		//in front of the single number
+		if(t.days<10){
+			days.innerHTML = '0' + t.days;
 		}else {
-			min.innerHTML = t.minutes;
+			days.innerHTML = t.days;
 		}
+		//If the remaining time gets to zero, stop the clock.
 		if(t.total<=0){
-			clearInterval(timeinterval);
+		  clearInterval(timeinterval);
 		}
-		if(!t.minutes) {
-			min.innerHTML = "--";
+		//If the days is not specified
+		if(!t.days) {
+			days.innerHTML = "--";
 		}
-		if(t.minutes<=0) {
-			min.innerHTML = "00";
+		//If there is no more days left
+		if(t.days<=0) {
+			days.innerHTML = "00";
 		}
 	},1000);
 }
-initializeMin('min', deadline);
+initializeDays('days', endtime);
 
 
-
+//Calculate the hours remaining, printing it
+//in the hours div
 function initializeHours(id, endtime){
 	var hours = document.getElementById(id);
 	var timeinterval = setInterval(function(){
@@ -138,57 +106,56 @@ function initializeHours(id, endtime){
 		}
 	},1000);
 }
-initializeHours('hours', deadline);
+initializeHours('hours', endtime);
 
 
-
-function initializeDays(id, endtime){
-	var days = document.getElementById(id);
+//Calculate the minutes remaining, printing it
+//in the min div
+function initializeMin(id, endtime){
+	var min = document.getElementById(id);
 	var timeinterval = setInterval(function(){
 		var t = getTimeRemaining(endtime);
-		if(t.days<10){
-			days.innerHTML = '0' + t.days;
+		if(t.minutes<10){
+			min.innerHTML = '0' + t.minutes;
 		}else {
-			days.innerHTML = t.days;
+			min.innerHTML = t.minutes;
 		}
 		if(t.total<=0){
-		  clearInterval(timeinterval);
+			clearInterval(timeinterval);
 		}
-		if(!t.days) {
-			days.innerHTML = "--";
+		if(!t.minutes) {
+			min.innerHTML = "--";
 		}
-		if(t.days<=0) {
-			days.innerHTML = "00";
+		if(t.minutes<=0) {
+			min.innerHTML = "00";
 		}
 	},1000);
 }
-initializeDays('days', deadline);
+initializeMin('min', endtime);
 
 
-//http://unbottle.getsandbox.com/campaign
+//Calculate the seconds remaining, printing it
+//in the sec div
+function initializeSec(id, endtime){
+	var sec = document.getElementById(id);
+	var timeinterval = setInterval(function(){
+		var t = getTimeRemaining(endtime);
+		if(t.seconds<10){
+			sec.innerHTML = '0' + t.seconds;
+		}else {
+			sec.innerHTML = t.seconds;
+		}
+		if(t.total<=0){
+			clearInterval(timeinterval);
+		}
+		if(!t.seconds) {
+			sec.innerHTML = "--";
+		}
+		if(t.seconds<=0) {
+			sec.innerHTML = "00";
+		}
+	},1000);
+}
+initializeSec('sec', endtime);
 
-//$.ajax({ 
-//    type: 'GET', 
-//    url: 'http://unbottle.getsandbox.com/campaign.json', 
-//    data: { data }, 
-//    dataType: 'json',
-//    success: function (data) { 
-//        $.each(data, function(index, element) {
-//            $('#test').append($('<div>', {
-//                text: element.name
-//            }));
-//        });
-//    }
-//});
-
-//$.getJSON(http://unbottle.getsandbox.com/campaign, function(data){
-//   console.log(data) // print array from resource
-//})
-
-//$.ajax({
-//   url: "http://unbottle.getsandbox.com/campaign",
-//   dataType: "json"
-//}).done(function( data ) {
-//  console.log( "Entire array:", data);
-//});
-
+})();
